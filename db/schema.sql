5600d3ca8c68920cc166ab44c090c2758e92c711
AndreasPapaz <Andreas@Andreas.local>
2017-05-10 20:52:31 -0500

    update database

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,11 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burger (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
     burger_name varchar(20),
-    devoured BOOLEAN,
-    date TIMESTAMP
+    devoured BOOLEAN DEFAULT false,
+    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
 );
 
-INSERT INTO burger (burger_name) VALUES ("McDouble");
-INSERT INTO burger (burger_name) VALUES ("WHOPPER");
-INSERT INTO burger (burger_name) VALUES ("cheese");
-INSERT INTO burger (burger_name) VALUES ("HAM");
