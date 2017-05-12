073540179f46b6a6fee86cd1d785b99296712d52
AndreasPapaz <Andreas@Andreas.local>
2017-05-12 12:33:02 -0500

    edit 2

--- config/connection.js
+++ config/connection.js
@@ -1,38 +1,29 @@
-var mysql = require('mysql');
-
-// var connection = mysql.createConnection({
-// 	port: 3306,
-// 	host: 'localhost',
-// 	user: 'root',
-// 	password: '',
-// 	database: 'burgers_db',
-// });
-
-// connection.connect(function(err) {
-// 	if (err) {
-// 		console.error("ERROR CODE : " + error.stack);
-// 	}
-// 	console.log("You are connected as ID : " + connection.threadId);
-// });
-
-//set up for JAWSdb
+//Import MySQL dependency
+var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
+//Define connection parameters
+
+if(process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-  });
-};
+	connection = mysql.createConnection({
+		port: '3306',
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	});
+}
 
+//Create connection
 connection.connect(function(error) {
-	if (error) {
-		console.log("conntion error: " +error.stack);
-		return;
-	}
+  if (error) {
+    console.error('Connection error: ' + error.stack);
+    return;
+  }
+  console.log('MySQL connection established! Connected as ID ' + connection.threadId);
 });
+
+// Export connection for ORM
 module.exports = connection;
