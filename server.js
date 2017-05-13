eea0215c16a411ae015a1f7c88418f541e1449d3
AndreasPapaz <Andreas@Andreas.local>
2017-05-12 20:45:11 -0500

    Heroku Up and Running. Need add functionality to refresh button

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 // var PORT = 3000;
 var app = express();
+//when i set the enviorment up i can use JAWS db
 app.set("port", (process.env.PORT || 3000));
 
 
@@ -21,13 +22,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main",}));
 app.set("view engine", "handlebars");
 
 
-
+//this works for JAWSdb and local host
 app.use('/', routes);
 app.listen(app.get("port"), function() {
 	console.log("You are running on port", app.get("port"));
 });
 
-
+//this works ONLY for local host. 
 // app.listen(PORT, function() {
 // 	console.log("Hey, You are connected on PORT : " + PORT);
 // 	console.log("Press cntrl + c to quit server");
